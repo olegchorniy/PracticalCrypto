12a4aff12a132b4b8972dfc646b72b93b9f5d8c0
Oleg <alig007@mail.ru>
2017-03-08 01:24:08 +0200

    Working version of Maurer's algorithm added.

--- key-generation/src/main/java/kpi/ipt/labs/practicalcrypto/Main.java
+++ key-generation/src/main/java/kpi/ipt/labs/practicalcrypto/Main.java
@@ -1,7 +1,16 @@
 package kpi.ipt.labs.practicalcrypto;
 
+import kpi.ipt.labs.practicalcrypto.primes.MaurerAlgorithm;
+
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        BigInteger p = MaurerAlgorithm.provablePrime(1024);
+
+        System.out.println(p);
+        System.out.println(p.isProbablePrime(100));
     }
 }
--- key-generation/src/main/java/kpi/ipt/labs/practicalcrypto/primes/MaurerAlgorithm.java
+++ key-generation/src/main/java/kpi/ipt/labs/practicalcrypto/primes/MaurerAlgorithm.java
@@ -1,17 +1,92 @@
 package kpi.ipt.labs.practicalcrypto.primes;
 
 import java.math.BigInteger;
+import java.util.Random;
 
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
+//TODO: add sieve for prime finding, replace j.u.Random with BM random (I guess?)
 public class MaurerAlgorithm {
 
     public static final int TRIAL_DIVISION_THRESHOLD = 20;
 
-    public static BigInteger provablePrime(int bitLength) {
+    //Maurer algorithm constants
+    private static final double C = 0.1;
+    private static final int M = 20;
+    private static final int DOUBLE_M = M * 2;
+    private static final BigInteger TWO = BigInteger.valueOf(2);
+    private static final BigInteger THREE = BigInteger.valueOf(3);
+
+    private static final Random rnd = new Random();
+
+    private static int recursionLevel = -1;
+
+    public static BigInteger provablePrime(final int bitLength) {
+        recursionLevel++;
+
+        print("Find prime of length: " + bitLength);
+
         if (bitLength <= TRIAL_DIVISION_THRESHOLD) {
-            return trialDivisionPrime(bitLength);
+            print("fallback to trivial division: " + bitLength);
+
+            BigInteger result = trialDivisionPrime(bitLength);
+            recursionLevel--;
+            return result;
         }
 
-        return null;
+        double B = C * bitLength * bitLength;
+        double r;
+        if (bitLength > DOUBLE_M) {
+            do {
+                r = 0.5 + rnd.nextDouble() / 2;
+            } while (bitLength * (1 - r) <= M);
+        } else {
+            r = 0.5;
+        }
+
+        print("r = " + r);
+
+        //recursive call
+        BigInteger q = provablePrime((int) (r * bitLength) + 1);
+
+        // floor( 2^(k - 1) / (2 * q) )
+        BigInteger I = ONE.shiftLeft(bitLength - 1).divide(q.shiftLeft(1));
+
+        boolean success = false;
+        BigInteger n = null;
+
+        print("Start loop ...");
+        while (!success) {
+            // R is random in the interval [I + 1, 2I]
+            BigInteger R = I.add(ONE).add(boundedRandom(I, rnd));
+            //n = 2Rq + 1
+            n = R.multiply(q).shiftLeft(1).add(ONE);
+
+            if (trialDivision(n, (long) B)) {
+                //a is random in the interval [2, n - 2] -> 2 + [0, n - 3)
+                BigInteger a = TWO.add(boundedRandom(n.subtract(THREE), rnd));
+                //b = a^(n - 1) mod n
+                BigInteger b = a.modPow(n.subtract(ONE), n);
+
+                if (b.equals(ONE)) {
+                    print("b equals 1");
+                    b = a.modPow(R.shiftLeft(1), n);
+                    BigInteger d = n.gcd(b.subtract(ONE));
+
+                    if (d.equals(ONE)) {
+                        success = true;
+                    } else {
+                        print("d = " + d);
+                    }
+                }
+            }
+        }
+
+        print("Found provable prime: " + n);
+
+        recursionLevel--;
+        return n;
     }
 
     private static BigInteger trialDivisionPrime(int bitLength) {
@@ -32,9 +107,46 @@ public class MaurerAlgorithm {
                 }
             }
 
+            print("Found by trial division prime: " + candidate);
             return BigInteger.valueOf(candidate);
         }
 
         throw new IllegalStateException("According to the Bertrand's postulate we should never reach this point");
+    }
+
+    private static boolean trialDivision(BigInteger testedNumber, long bound) {
+        //check whether testedNumber is divisible by 2
+        if (!testedNumber.testBit(0)) {
+            return false;
+        }
+
+        for (int d = 3; d < bound; d += 2) {
+            if (testedNumber.mod(BigInteger.valueOf(d)).equals(ZERO)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static BigInteger boundedRandom(BigInteger bound, Random rnd) {
+        int maxLength = bound.bitLength();
+
+        BigInteger b;
+        do {
+            b = new BigInteger(maxLength, rnd);
+        } while (b.compareTo(bound) >= 0);
+
+        return b;
+    }
+
+    private static void print(String message) {
+        for (int i = 0; i < recursionLevel; i++) {
+            if (i == recursionLevel - 1)
+                System.out.print("|--");
+            else
+                System.out.print("|  ");
+        }
+        System.out.println(message);
     }
 }
